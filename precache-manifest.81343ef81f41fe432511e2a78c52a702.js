9e352d89d7e5a475d6239fd3f474b2305b00fd53
Serhii Shulha <sagemini17@gmail.com>
2019-10-11 20:38:57 +0300

    Updates

---
+++ precache-manifest.81343ef81f41fe432511e2a78c52a702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b09be79fe5ba21c449f55f0e34c6fe4",
+    "url": "/tooltip-test/index.html"
+  },
+  {
+    "revision": "89fe417013057e54ae0a",
+    "url": "/tooltip-test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "240039ec1dc5fabe25bc",
+    "url": "/tooltip-test/static/js/2.222d9409.chunk.js"
+  },
+  {
+    "revision": "89fe417013057e54ae0a",
+    "url": "/tooltip-test/static/js/main.94c7adf2.chunk.js"
+  },
+  {
+    "revision": "b40509f9dc9647c4f5b4",
+    "url": "/tooltip-test/static/js/runtime-main.e1fd1d1b.js"
+  }
+]);
